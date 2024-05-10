795b30a5cecf9e3824b0f0b267f8dc226fe2f261
Sam35203737 <mrityunjaysamdarshi3737@gmail.com>
2024-05-10 14:22:55 +0530

    My new Feature

---
+++ Subtraction.java
@@ -0,0 +1,14 @@
+package LocalRepo;
+
+public class Subtraction {
+ public static void main(String[]args){
+    int a=10;
+    int b=5;
+    int c=a-b;
+    System.out.println(c);
+    multiply(a, b);
+ }
+ public static void multiply(int a,int b){
+    System.out.println(a*b);
+ }
+}
